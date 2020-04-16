97cf8920847e1e12a7100bde09c7f6c4aeecfd6c
Mike <tankf33der@disroot.org>
2020-04-16 10:39:25 +0300

    .

--- keccak/main.c
+++ keccak/main.c
@@ -3,22 +3,26 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <string.h>
-
-extern int keccak(const uint8_t *in, size_t inlen, uint8_t *md, size_t mdlen);
+#include "hmac-keccak.h"
 
 int main(void)
 {
     printf("start\n");
-    uint8_t b[200];
+    uint8_t r[32];
+    uint8_t k[32];
     uint8_t m[256];
 
     for (size_t i = 0; i < 256; i++) {
         m[i] = i;
     }
-
-    for (size_t i = 0; i < 256; i++) {
-        keccak(m, i, b, 200);
+    for (size_t i = 0; i < 32; i++) {
+        k[i] = i;
     }
-
+    for (size_t i = 0; i < 256; i++) {
+        hmac_keccak_hash(r, k, 0, m, i);
+    }
+    for (size_t i = 0; i < 32; i++) {
+        hmac_keccak_hash(r, k, i, m, 256);
+    }
     return 0;
 }
